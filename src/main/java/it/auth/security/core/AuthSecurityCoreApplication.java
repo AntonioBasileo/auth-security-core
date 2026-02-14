749ee6557328bdd83b8476cffc1b704fbfbdb786
antoniobasileo <antonio.basileo92@gmail.com>
2026-02-14 02:32:32 +0100

    minor fix

--- src/main/java/it/auth/security/core/AuthSecurityCoreApplication.java
+++ src/main/java/it/auth/security/core/AuthSecurityCoreApplication.java
@@ -1,12 +1,10 @@
 package it.auth.security.core;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @AutoConfiguration
 @ComponentScan(basePackages = "it.auth.security.core")
-@EntityScan(basePackages = "it.auth.security.core.entity")
 public class AuthSecurityCoreApplication{
 
 }
